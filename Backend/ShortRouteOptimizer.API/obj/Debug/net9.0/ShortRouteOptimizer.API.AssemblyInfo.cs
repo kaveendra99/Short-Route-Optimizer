cc69ff1c8236a7a2ed826511c2ff0a57d6d41377
kaveendra wijenayake <65034521+kaveendra99@users.noreply.github.com>
2025-10-16 22:49:45 +0530

    last commit

--- Backend/ShortRouteOptimizer.API/obj/Debug/net9.0/ShortRouteOptimizer.API.AssemblyInfo.cs
+++ Backend/ShortRouteOptimizer.API/obj/Debug/net9.0/ShortRouteOptimizer.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShortRouteOptimizer.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5854d2d8498d7f7cd76f866a1e621ba364ecfa8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShortRouteOptimizer.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShortRouteOptimizer.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
